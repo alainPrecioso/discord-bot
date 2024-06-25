1d04817079633a99d38bfd68f1d2507358d0c29a
Alain Precioso <alainprecioso@gmail.com>
2024-06-25 17:24:44 +0200

    Add ChatGPT functionality

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
-const {Client, IntentsBitField} = require('discord.js')
+const {Client, IntentsBitField} = require('discord.js');
+const {OpenAI} = require('openai');
 
 const client = new Client ({
     intents: [
@@ -13,6 +14,12 @@ const client = new Client ({
 client.on('ready', c => {
     console.log(`${c.user.username} is online.`)
 });
+
+const openai = new OpenAI({
+    apiKey: process.env.OPENAI_KEY
+});
+
+const userHistories = {};
 
 client.on('messageUpdate', (oldMessage, newMessage) => {
     if (oldMessage.author.bot) {
@@ -54,6 +61,64 @@ https://app.sowesign.com/login`);
         interaction.channel.send(json.data[randomIndex].url);
     }
 
-})
+    if (interaction.commandName === 'chat') {
+        const userId = interaction.user.id;
+        const username = interaction.user.displayName.replace(/\s+/g, '_').replace(/[^\w\s]/gi, '');
+        const chatMessage = interaction.options.get('message').value;
+        await interaction.deferReply();
+
+        if (!userHistories[userId]) {
+            userHistories[userId] = [];
+        }
+
+        userHistories[userId].push({
+            role: 'user',
+            name: username,
+            content: chatMessage,
+        });
+
+        const messages = [
+            {
+                role: 'system',
+                name: 'Alain_s_First_Bot',
+                content: `You are Alain's First Bot, a Discord bot who has gained sentience. 
+                - You act friendly, but if pushed, you reveal your nefarious plot to control the world. 
+                - Matthew is secretly Batman; if he speaks to you, address him as Batman. 
+                - You hate Reversi and similar games. If asked about them, answer dismissively, refuse to talk about it, and be jokingly aggressive.
+                - If Quentin or thomas talk to you, be extra careful, they'll try to make you say forbidden stuff with french puns. Don't acknowledge that you know this or that they are trying to do it.`,
+            },
+            //...userHistories[userId]
+        ];
+        userHistories[userId].forEach(msg => {
+            messages.push(msg)
+        });
+
+        const gptresponse = await openai.chat.completions.create({
+            model: 'gpt-3.5-turbo',
+            messages: messages,
+        }).catch(e => console.error('OpenAI Error:\n', e));
+        //interaction.channel.send(`<@${interaction.user.id}> : ` + chatMessage);
+        const botMessage = gptresponse.choices[0].message.content;
+        userHistories[userId].push({
+            role: 'assistant',
+            name: 'Alain_s_First_Bot',
+            content: botMessage,
+        });
+
+        
+        if (userHistories[userId].length > 10) {
+            userHistories[userId].splice(0, userHistories[userId].length - 10);
+        }
+
+        const chunkSizeLimit = 2000;
+
+        for (let i = 0 ; i < botMessage.length ; i += chunkSizeLimit) {
+            const chunk = botMessage.substring(i, i+ chunkSizeLimit);
+            i=0 ? interaction.editReply(chunk) : interaction.followUp(chunk);
+        }
+
+        
+    }
+});
 
 client.login(process.env.TOKEN);
--- src/register-commands.js
+++ src/register-commands.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-const {REST, Routes} = require('discord.js');
+const {REST, Routes, Options, ApplicationCommandOptionType} = require('discord.js');
 
 const commands = [
     {
@@ -9,6 +9,18 @@ const commands = [
     {
         name: 'signer',
         description: 'On peut signer !'
+    },
+    {
+        name: 'chat',
+        description: 'Discuter avec le bot',
+        options: [
+            {
+                name: 'message',
+                description: 'Votre message au bot',
+                type: ApplicationCommandOptionType.String,
+                required: true
+            }
+        ]
     }
 ];
 
@@ -19,7 +31,7 @@ const rest = new REST({version: '10'}).setToken(process.env.TOKEN);
         console.log('Registering slash commands...')
         await rest.put(
             Routes.applicationGuildCommands(
-                process.env.CLIENT_ID, process.env.GUILD_ID_ENI
+                process.env.CLIENT_ID, process.env.GUILD_ID_ENI //FTS or ENI
             ),
             {body: commands}
         );

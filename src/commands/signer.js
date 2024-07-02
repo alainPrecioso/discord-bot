326052f67c8f9cde3d844ed7e92cb9985fe4e891
Alain Precioso <alainprecioso@gmail.com>
2024-07-02 07:40:03 +0200

    Show proper attribution to Tenor with embed GIF

--- src/commands/signer.js
+++ src/commands/signer.js
@@ -1,15 +1,5 @@
 const fetch = require('node-fetch');
-const gifs = [
-    'https://giphy.com/gifs/CBSAllAccess-fight-good-the-oyGpShr0FL7Fmdu1CG',
-    'https://tenor.com/view/waiver-sighn-signature-risky-gif-10443165282523180661',
-    'https://tenor.com/view/breakup-peach-and-goma-angry-gif-24105232',
-    'https://tenor.com/view/rajini-signature-cute-gif-15784204',
-    'https://tenor.com/view/homer-simpsons-signature-gif-10450721',
-    'https://tenor.com/view/donte-hall-taedatea-donth8tae-dear-diary-writing-gif-20012396',
-
-
-]
-
+const { EmbedBuilder } = require('discord.js');
 
 module.exports = {
     name: 'signer',
@@ -22,12 +12,11 @@ module.exports = {
         const url = `https://tenor.googleapis.com/v2/search?q=${search_term}&key=${apikey}&client_key=${clientkey}&limit=${lmt}&media_filter=gif`;
        
         interaction.reply(`@everyone, on peut signer ! 
-https://app.sowesign.com/login`);
+[SoWeSign](https://app.sowesign.com/login)`);
 
         try {
             const res = await fetch(url);
             const json = await res.json();
-            console.log(json)
             const filteredResults = json.results.filter(result => {
                 const excludedKeywords = ['hancock', 'benjammins', 'dance', 'contract signature', 'contract official'];
                 return !excludedKeywords.some(keyword => 
@@ -36,10 +25,18 @@ https://app.sowesign.com/login`);
             });
             const randomIndex = Math.floor(Math.random() * filteredResults.length);
             const gifUrl = filteredResults[randomIndex].media_formats.gif.url;
-            interaction.channel.send(gifUrl);
+
+            const embed = new EmbedBuilder()
+                .setColor('#FF9900')
+                //.setTitle('Signez ici :')
+                //.setDescription(`[SoWeSign](https://app.sowesign.com/login)`)
+                .setImage(gifUrl)
+                .setFooter({text:'Via Tenor - Check out more GIFs on https://tenor.com', iconURL:'https://tenor.com/favicon.ico'})
+                ;
+
+            interaction.channel.send({ embeds: [embed] });
         } catch (error) {
             console.error('Error fetching data from Tenor:', error);
-            interaction.reply('Sorry, something went wrong while fetching the GIF.');
         }
     },
 };
